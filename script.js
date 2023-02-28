046f8037ab3f428ea7c2fb16f1acbe1a36e50e5d
Ankit_S <112768684+Ankitsreenivasa@users.noreply.github.com>
2023-02-28 19:54:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Mobile menu
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+const hLink = document.querySelectorAll("#hLink");
+const faSolid = document.querySelector(".fa-solid");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  faSolid.classList.toggle("fa-xmark");
+});
