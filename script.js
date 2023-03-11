da7b571175f6ba2ab8cf923252879ac6e84136a7
Ankit_S <112768684+Ankitsreenivasa@users.noreply.github.com>
2023-03-11 09:07:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,4 @@ const menu = document.querySelector("#menu");
 const hLink = document.querySelectorAll("#hLink");
 const faSolid = document.querySelector(".fa-solid");
 
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-  faSolid.classList.toggle("fa-xmark");
-})
+
