4d72b06e1d1f7ff1173bdcfdf432be38d3c22511
Ankit_S <112768684+Ankitsreenivasa@users.noreply.github.com>
2023-03-11 09:07:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
 const hLink = document.querySelectorAll("#hLink");
-const faSolid = document.querySelector(".fa-solid");
+
 
 
